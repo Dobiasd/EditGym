0f0a52f4f3b601695bf245488ac26727e0e808c6
Dobiasd <harry@daiw.de>
2014-12-06 22:39:06 +0100

    filter keys

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -11,7 +11,25 @@ type State = { editor : Editor.State, keysDown : Set.Set Int }
 type Input = { inKeysDown : Set.Set Int }
 
 input : Signal Input
-input = (Input << Set.fromList) <~ Keyboard.keysDown
+input = (Input << Set.fromList << filter validKey) <~ Keyboard.keysDown
+
+validKey : Int -> Bool
+validKey key =
+  if | key >= 65 && key <= 90 -> True -- a to z
+     | key >= 37 && key <= 40 -> True -- arrow keys
+     | key == 16 -> True -- shift
+     | key == 17 -> True -- ctrl
+     | key == 32 -> True -- space
+     | key >= 48 && key <= 57 -> True -- top numbers
+     | key >= 96 && key <= 105 -> True -- block numbers
+     | key == 13 -> True -- enter
+     | key == 33 || key == 34 -> True -- page up, page down
+     | key == 35 || key == 36 -> True -- end, pos1
+     | key == 106 || key == 107 || key == 109 -> True -- *, +, -
+     | key == 188 || key == 190 -> True -- , and .
+     | key == 46 -> True -- delete
+     | key == 8 -> True -- backspace
+     | otherwise -> True
 
 initialState : State
 initialState = State Editor.initialState Set.empty
