c4c2da84f9890108056c04514f50585711a37d5e
Dobiasd <harry@daiw.de>
2014-12-09 21:00:09 +0100

    shorter share messages

--- src/elm/Header.elm
+++ src/elm/Header.elm
@@ -24,15 +24,15 @@ shareIcons =
   let
     buttons =
       [ ( image iconSize iconSize "imgs/facebook.png", "https://www.facebook.com/sharer/sharer.php?u=http://www.editgym.com" )
-      , ( image iconSize iconSize "imgs/twitter.png", "https://twitter.com/home?status=Text%20editing%20training%20with%20highscores%20and%20instant%20replay%20at%20http://www.editgym.com" )
+      , ( image iconSize iconSize "imgs/twitter.png", "https://twitter.com/home?status=Text%20editing%20training%20at%20http://www.editgym.com" )
       , ( image iconSize iconSize "imgs/googleplus.png", "https://plus.google.com/share?url=http://www.editgym.com" )
-      , ( image iconSize iconSize "imgs/linkedin.png", "https://www.linkedin.com/shareArticle?mini=true&url=http://www.editgym.com&title=Text%20editing%20training%20with%20highscores%20and%20instant%20replay&source=" )
-      , ( image iconSize iconSize "imgs/pinterest.png", "https://pinterest.com/pin/create/button/?url=&media=http://www.editgym.com&description=Text%20editing%20training%20with%20highscores%20and%20instant%20replay" )
-      , ( image iconSize iconSize "imgs/digg.png", "http://digg.com/submit?phase=2&url=http://www.editgym.com&title=Text%20editing%20training%20with%20highscores%20and%20instant%20replay" )
-      , ( image iconSize iconSize "imgs/stumbleupon.png", "http://www.stumbleupon.com/submit?url=http://www.editgym.com&title=Text%20editing%20training%20with%20highscores%20and%20instant%20replay" )
+      , ( image iconSize iconSize "imgs/linkedin.png", "https://www.linkedin.com/shareArticle?mini=true&url=http://www.editgym.com&title=Text%20editing%20training&source=" )
+      , ( image iconSize iconSize "imgs/pinterest.png", "https://pinterest.com/pin/create/button/?url=&media=http://www.editgym.com&description=Text%20editing%20training" )
+      , ( image iconSize iconSize "imgs/digg.png", "http://digg.com/submit?phase=2&url=http://www.editgym.com&title=Text%20editing%20training" )
+      , ( image iconSize iconSize "imgs/stumbleupon.png", "http://www.stumbleupon.com/submit?url=http://www.editgym.com&title=Text%20editing%20training" )
       , ( image iconSize iconSize "imgs/tumblr.png", "http://www.tumblr.com/share/link?url=editgym.com" )
-      , ( image iconSize iconSize "imgs/bufferapp.png", "https://bufferapp.com/add?url=http://www.editgym.com&text=Text%20editing%20training%20with%20highscores%20and%20instant%20replay" )
-      , ( image iconSize iconSize "imgs/email.png", "mailto:%20?subject=reddit time machine&body=Text%20editing%20training%20with%20highscores%20and%20instant%20replayat%20http://www.editgym.com" ) ]
+      , ( image iconSize iconSize "imgs/bufferapp.png", "https://bufferapp.com/add?url=http://www.editgym.com&text=Text%20editing%20training" )
+      , ( image iconSize iconSize "imgs/email.png", "mailto:%20?subject=reddit time machine&body=Text%20editing%20training%20at%20http://www.editgym.com" ) ]
       |> map (\ (img, url) -> img |> link url)
   in
     toDefText "share: " :: buttons |> intersperse (defaultSpacer) |> flow right
