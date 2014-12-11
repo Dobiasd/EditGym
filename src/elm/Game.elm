5e2f27f5eb893cdcc295611d573207513e24cc49
Dobiasd <harry@daiw.de>
2014-12-11 22:25:49 +0100

    string representation of keys

--- src/elm/Game.elm
+++ src/elm/Game.elm
@@ -31,30 +31,17 @@ state = foldp step initialState input
 step : Input -> State -> State
 step {inKeysDown} ({editor, keyHistory, keysDown} as state) =
   let keysDownNew = Set.diff inKeysDown keysDown
+      keysUpNew = Set.diff keysDown inKeysDown
   in  { state | editor <- Editor.step editor keysDown keysDownNew
-              , keyHistory <- KeyHistory.step keyHistory keysDown keysDownNew
+              , keyHistory <- KeyHistory.step keyHistory keysDown
+                                              keysDownNew keysUpNew
               , keysDown <- inKeysDown }
 
 main : Signal Element
 main = scene <~ Window.width ~ Window.height ~ state
 
 validKey : Int -> Bool
-validKey key =
-  if | key >= 65 && key <= 90 -> True -- a to z
-     | key >= 37 && key <= 40 -> True -- arrow keys
-     | key == 16 -> True -- shift
-     | key == 17 -> True -- ctrl
-     | key == 32 -> True -- space
-     | key >= 48 && key <= 57 -> True -- top numbers
-     | key >= 96 && key <= 105 -> True -- block numbers
-     | key == 13 -> True -- enter
-     -- | key == 33 || key == 34 -> True -- page up, page down
-     | key == 35 || key == 36 -> True -- end, pos1
-     | key == 106 || key == 107 || key == 109 -> True -- *, +, -
-     | key == 188 || key == 190 -> True -- , and .
-     | key == 46 -> True -- delete
-     | key == 8 -> True -- backspace
-     | otherwise -> True
+validKey key = KeyHistory.showKey key /= ""
 
 scene : Int -> Int -> State -> Element
 scene w h {editor, keyHistory} =
--- src/elm/KeyHistory.elm
+++ src/elm/KeyHistory.elm
@@ -2,26 +2,67 @@ module KeyHistory where
 
 import Keyboard
 import Set
+import Dict
 import Char(fromCode)
 import String(cons)
 
-import Layout (toDefText)
+import Layout (toDefText, toColText, white1, lightGray1, darkGray1)
 
-type State = {history:[Int]}
+data KeyAction = Up Int | Down Int
+
+showKeyAction : KeyAction -> Element
+showKeyAction action = case action of
+  Up key   -> showKey key |> toColText lightGray1
+  Down key -> showKey key |> toColText white1
+
+type State = {history:[KeyAction]}
 
 initialState : State
 initialState = State []
 
+keyStrings : Dict.Dict Int String
+keyStrings =
+  let toDefStrPair mod key = (mod key, key |> fromCode |> (flip cons) "")
+  in  [
+    (37, "left")
+  , (38, "up")
+  , (39, "right")
+  , (40, "down")
+  , (16, "shift")
+  , (17, "ctrl")
+  , (32, "space")
+  , (13, "enter")
+  , (35, "end")
+  , (36, "pos1")
+  , (106, "*")
+  , (107, "+")
+  , (109, "-")
+  , (188, ",")
+  , (190, "-")
+  , (173, "-")
+  , (46, "del")
+  , (8, "bs")
+  ]
+  ++ (map (toDefStrPair identity) [48..57]) -- top numbers
+  ++ (map (toDefStrPair identity) [96..105]) -- block numbers
+  ++ (map (toDefStrPair (\x -> x - 32)) [97..122]) -- a to z
+  |> Dict.fromList
+
 -- todo: use String.fromChar
 showKey : Int -> String
-showKey key = key |> fromCode |> (flip cons) ""
+showKey key = case Dict.get key keyStrings of
+  Just result -> result
+  Nothing -> ""
+  --Nothing -> key |> fromCode |> (flip cons) ""
 
-step : State -> Set.Set Int -> Set.Set Int -> State
-step ({history} as state) keysDown keysDownNew =
-  let history' = history ++ (Set.toList keysDownNew) -- todo: key combinations
+step : State -> Set.Set Int -> Set.Set Int -> Set.Set Int -> State
+step ({history} as state) keysDown keysDownNew keysUpNew =
+  let history' = history ++ (Set.toList keysDownNew |> map Down)
+                         ++ (Set.toList keysUpNew   |> map Up)
   in  { state | history <- history' }
 
 display : State -> Element
 display {history} =
-  let visibleHistory = drop (length history - 5) history
-  in  show (map showKey visibleHistory) |> toDefText
+  let visibleHistory = drop (length history - 30) history
+      sep = spacer 2 20 |> color darkGray1
+  in  map showKeyAction visibleHistory |> intersperse sep |> flow right
