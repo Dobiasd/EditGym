b456700a5637d0a0e9f14cabcd262b37133b4d0e
Dobiasd <harry@daiw.de>
2014-12-14 12:15:09 +0100

    time measurement

--- src/elm/Game.elm
+++ src/elm/Game.elm
@@ -3,6 +3,9 @@ module Game where
 import Window
 import Set
 import Keyboard
+import Time
+import List
+import String
 
 import Layout (toDefText, toSizedText, lightGray1)
 import Skeleton
@@ -14,6 +17,7 @@ type State = {
   , keyHistory : KeyHistory.State
   , keysDown : Set.Set Keyboard.KeyCode
   , goal : String
+  , timeInDs : Int
   }
 
 port start : Signal String
@@ -24,8 +28,11 @@ initialState = State Editor.initialState
                      KeyHistory.initialState
                      Set.empty
                      "loading"
+                     0
 
-data Input = Start String | Goal String | Keys (Set.Set Keyboard.KeyCode)
+data Input = Decisecond | Start String
+                        | Goal String
+                        | Keys (Set.Set Keyboard.KeyCode)
 
 startInput : Signal Input
 startInput = Start <~ start
@@ -36,8 +43,15 @@ goalInput = Goal <~ goal
 keyInput : Signal Input
 keyInput = Keys <~ ((Set.fromList << filter validKey) <~ Keyboard.keysDown)
 
+
+{-| We want the time to update every 100 milliseconds if possible. -}
+ticker = lift (\t -> t / 1000) <| fps 10
+
+timeInput : Signal Input
+timeInput = (always Decisecond) <~ every (100 * millisecond)
+
 input : Signal Input
-input = merges [startInput, goalInput, keyInput]
+input = merges [startInput, goalInput, keyInput, timeInput]
 
 state : Signal State
 state = foldp step initialState input
@@ -48,6 +62,13 @@ step input =
     Start start -> setStart start
     Goal goal -> setGoal goal
     Keys keys -> stepKeys keys
+    Decisecond -> stepDecisecond
+
+stepDecisecond : State -> State
+stepDecisecond ({editor, timeInDs, keyHistory, goal} as state) =
+  if editor.document == goal || List.isEmpty keyHistory.history
+    then state
+    else { state | timeInDs <- timeInDs + 1 }
 
 setStart : String -> State -> State
 setStart start ({editor} as state) =
@@ -78,10 +99,14 @@ displayGoal : String -> Element
 displayGoal goal = Editor.displayTextCol lightGray1 goal
 
 scene : Int -> Int -> State -> Element
-scene w h ({editor, keyHistory, editor, goal} as state) =
+scene w h ({editor, keyHistory, editor, goal, timeInDs} as state) =
   let finished = editor.document == goal
       result = if finished
-                 then length keyHistory.history |> show |> toSizedText 256
+                 then flow down [
+                     length keyHistory.history |> show |> toSizedText 180
+                   , spacer 1 10
+                   , showTime timeInDs |> toSizedText 180
+                   ]
                  else empty
       content = flow outward [
                     scenePlay w h state
@@ -89,22 +114,41 @@ scene w h ({editor, keyHistory, editor, goal} as state) =
                   ]
   in  Skeleton.showPage w h content
 
+showTime : Int -> String
+showTime timeInDs =
+  let str = toFloat timeInDs / 10 |> show
+      str' = if (String.right 2 str |> String.left 1) == "."
+               then str
+               else String.append str ".0"
+  in  String.append str' "s"
+
+displayTime : Int -> Element
+displayTime timeInDs =
+   showTime timeInDs |> toSizedText 48
+
+-- todo: fixed widths
 scenePlay : Int -> Int -> State -> Element
-scenePlay w h {editor, keyHistory, goal} =
+scenePlay w h {editor, keyHistory, goal, timeInDs} =
   flow down [
       spacer 800 1
-    , KeyHistory.display keyHistory
     , flow right [
-         flow down [
-             "editor" |> toSizedText 48
-           , spacer 1 30
-           , Editor.display editor
-         ]
-       , spacer 100 1
-       , flow down [
-             "goal" |> toSizedText 48
-           , spacer 1 30
-           , displayGoal goal
-       ]
-    ]
+          KeyHistory.display keyHistory
+        , spacer 200 1
+        , displayTime timeInDs
+      ]
+    , flow right [
+          flow down [
+              spacer 400 1
+            , "editor" |> toSizedText 48
+            , spacer 1 30
+            , Editor.display editor
+          ]
+        , spacer 100 1
+        , flow down [
+              spacer 400 1
+            , "goal" |> toSizedText 48
+            , spacer 1 30
+            , displayGoal goal
+          ]
+      ]
   ]
