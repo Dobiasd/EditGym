baab3cb98a3abcbce18fbf35b18ad6a4024df199
Dobiasd <harry@daiw.de>
2014-12-15 10:42:49 +0100

    generate share links with UrlEncode

--- src/elm/Header.elm
+++ src/elm/Header.elm
@@ -2,6 +2,7 @@ module Header where
 
 import Graphics.Input (Input, input)
 import Window
+import UrlEncode(genLink)
 
 import Layout (defaultSpacer, toDefText, toSizedText,
   black1, white1, orange1, blue1, purple1, red1, green1, gray1)
@@ -23,16 +24,16 @@ shareIcons : Element
 shareIcons =
   let
     buttons =
-      [ ( image iconSize iconSize "imgs/facebook.png", "https://www.facebook.com/sharer/sharer.php?u=http://www.editgym.com" )
-      , ( image iconSize iconSize "imgs/twitter.png", "https://twitter.com/home?status=Text%20editing%20training%20at%20http://www.editgym.com" )
-      , ( image iconSize iconSize "imgs/googleplus.png", "https://plus.google.com/share?url=http://www.editgym.com" )
-      , ( image iconSize iconSize "imgs/linkedin.png", "https://www.linkedin.com/shareArticle?mini=true&url=http://www.editgym.com&title=Text%20editing%20training&source=" )
-      , ( image iconSize iconSize "imgs/pinterest.png", "https://pinterest.com/pin/create/button/?url=&media=http://www.editgym.com&description=Text%20editing%20training" )
-      , ( image iconSize iconSize "imgs/digg.png", "http://digg.com/submit?phase=2&url=http://www.editgym.com&title=Text%20editing%20training" )
-      , ( image iconSize iconSize "imgs/stumbleupon.png", "http://www.stumbleupon.com/submit?url=http://www.editgym.com&title=Text%20editing%20training" )
-      , ( image iconSize iconSize "imgs/tumblr.png", "http://www.tumblr.com/share/link?url=editgym.com" )
-      , ( image iconSize iconSize "imgs/bufferapp.png", "https://bufferapp.com/add?url=http://www.editgym.com&text=Text%20editing%20training" )
-      , ( image iconSize iconSize "imgs/email.png", "mailto:%20?subject=EditGym&body=Text%20editing%20training%20at%20http://www.editgym.com" ) ]
+      [ ( image iconSize iconSize "imgs/facebook.png", genLink "https://www.facebook.com/sharer/sharer.php" [("u", "http://www.editgym.com")] )
+      , ( image iconSize iconSize "imgs/twitter.png", genLink "https://twitter.com/home" [("status", "Text editing training at http://www.editgym.com")] )
+      , ( image iconSize iconSize "imgs/googleplus.png", genLink "https://plus.google.com/share" [("url", "http://www.editgym.com")] )
+      , ( image iconSize iconSize "imgs/linkedin.png", genLink "https://www.linkedin.com/shareArticle" [("mini", "true"), ("url", "http://www.editgym.com"), ("title", "Text editing training"), ("source", "" )] )
+      , ( image iconSize iconSize "imgs/pinterest.png", genLink "https://pinterest.com/pin/create/button/" [("url", ""), ("media", "http://www.editgym.com"), ("description", "Text editing training")] )
+      , ( image iconSize iconSize "imgs/digg.png", genLink "http://digg.com/submit" [("phase", "2"), ("url", "http://www.editgym.com"), ("title", "Text editing training")] )
+      , ( image iconSize iconSize "imgs/stumbleupon.png", genLink "http://www.stumbleupon.com/submit" [("url", "http://www.editgym.com"), ("title", "Text editing training")] )
+      , ( image iconSize iconSize "imgs/tumblr.png", genLink "http://www.tumblr.com/share/link" [("url", "editgym.com")] )
+      , ( image iconSize iconSize "imgs/bufferapp.png", genLink "https://bufferapp.com/add" [("url", "http://www.editgym.com"), ("text", "Text editing training")] )
+      , ( image iconSize iconSize "imgs/email.png", genLink "mailto: " [("subject", "EditGym"), ("body", "Text editing training at http://www.editgym.com")] ) ]
       |> map (\ (img, url) -> img |> link url)
   in
     toDefText "share: " :: buttons |> intersperse (defaultSpacer) |> flow right
--- src/elm/UrlEncode.elm
+++ src/elm/UrlEncode.elm
@@ -1,11 +1,24 @@
 module UrlEncode where
 
+import Dict
+import List
+import String
+
 -- http://en.wikipedia.org/w/index.php?title=Percent-encoding&oldid=634660400#Types_of_URI_characters
 unreservedCharacters =
   [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_', '.', '~' ]
 
+charToString : Char -> String
+charToString c = String.fromList [c]
+
+unreservedCharactersDict : Dict.Dict Char String
+unreservedCharactersDict =
+  zip unreservedCharacters (List.map charToString unreservedCharacters)
+  |> Dict.fromList
+
 -- http://en.wikipedia.org/w/index.php?title=Percent-encoding&oldid=634660400#Percent-encoding_reserved_characters
-reservedCharacters = [
+reservedCharactersDict : Dict.Dict Char String
+reservedCharactersDict = [
     ('!', "%21")
   , ('#', "%23")
   , ('$', "%24")
@@ -24,14 +37,15 @@ reservedCharacters = [
   , ('@', "%40")
   , ('[', "%5B")
   , (']', "%5D")
-  ]
+  ] |> Dict.fromList
 
 -- http://en.wikipedia.org/w/index.php?title=Percent-encoding&oldid=634660400#Character_data
-commonCharacters = [
+commonCharactersDict : Dict.Dict Char String
+commonCharactersDict = [
     ('\n', "%0A")
   , ('\r', "%0D")
-  , ('"', "%20")
-  , ('', "%22")
+  , (' ', "%20")
+  , ('"', "%22")
   , ('%', "%25")
   , ('-', "%2D")
   , ('.', "%2E")
@@ -45,4 +59,29 @@ commonCharacters = [
   , ('|', "%7C")
   , ('}', "%7D")
   , ('~', "%7E")
-  ]
+  ] |> Dict.fromList
+
+charactersDict : Dict.Dict Char String
+charactersDict =
+  commonCharactersDict
+  |> Dict.union reservedCharactersDict
+  |> Dict.union unreservedCharactersDict
+
+encodeChar : Char -> String
+encodeChar c = Dict.getOrElse "-" c charactersDict
+
+encode : String -> String
+encode =
+  String.toList
+  >> List.map encodeChar
+  >> List.map String.toList
+  >> List.concat
+  >> String.fromList
+
+genLink : String -> [(String, String)] -> String
+genLink baseUrl parameters =
+  let
+    paramsUrl = map (\(name, value) -> name ++ "=" ++ encode value) parameters
+              |> join "&"
+  in
+    baseUrl ++ (if String.isEmpty paramsUrl then "" else "?" ++ paramsUrl)
