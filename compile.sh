479ee22e6303a1cb5cfb2bfbee7450b77d287af4
Dobiasd <harry@daiw.de>
2014-12-16 00:23:32 +0100

    clean up

--- compile.sh
+++ compile.sh
@@ -5,33 +5,16 @@ rm -r build
 mkdir build
 mkdir build/js
 
-#elm-make src/elm/Start.elm --output build/js/Start.js
-#elm-make src/elm/Levels.elm --output build/js/Levels.js
-#elm-make src/elm/Game.elm --output build/js/Game.js
-#elm-make src/elm/Highscores.elm --output build/js/Highscores.js
-#elm-make src/elm/Contact.elm --output build/js/Contact.js
-#elm-make src/elm/Help.elm --output build/js/Help.js
-#elm-make src/elm/FAQ.elm --output build/js/FAQ.js
-elm-make src/elm/Start.elm src/elm/Levels.elm src/elm/Game.elm src/elm/Highscores.elm src/elm/Contact.elm src/elm/Help.elm src/elm/FAQ.elm --output build/js/Bundle.js
+elm-make src/elm/Start.elm src/elm/Levels.elm src/elm/Game.elm src/elm/Highscores.elm src/elm/Contact.elm src/elm/Help.elm src/elm/FAQ.elm --output build/js/Bundle_uncompressed.js
 
 if [ $? -eq 0 ]
 then
 
   cp -r ./src/imgs ./build
-
-  mkdir -p ./build/js
-
-  #uglifyjs $HOME/.cabal/share/Elm-0.13/elm-runtime.js > ./build/js/elm-runtime.js
-
-  for pathname in ./build/js/*.js
-  do
-    filename="${pathname##*/}"
-    uglifyjs "$pathname" > "$pathname.ugly"
-    rm "$pathname"
-    mv "$pathname.ugly" "$pathname"
-  done
-
   cp -R ./src/levels ./build
+
+  uglifyjs build/js/Bundle_uncompressed.js > build/js/Bundle.js
+  rm build/js/Bundle_uncompressed.js
 
   cp ./src/index.html ./build/index.html
   uglifyjs ./src/htmlMain.js > ./build/js/htmlMain.js
