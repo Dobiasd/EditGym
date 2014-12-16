5b7a54e68f473282b64c51bc02ef4334a2067877
Dobiasd <harry@daiw.de>
2014-12-16 22:27:31 +0100

    clean up

--- src/elm/KeyHistory.elm
+++ src/elm/KeyHistory.elm
@@ -31,29 +31,29 @@ initialState = State []
 
 keyStrings : Dict.Dict Keyboard.KeyCode String
 keyStrings =
-  let toDefStrPair mod key = (mod key, key |> fromCode |> (flip cons) "")
+  let strFromCode = fromCode >> (flip cons) ""
   in  [
-    (37, "left")
-  , (38, "up")
-  , (39, "right")
-  , (40, "down")
-  , (16, "shift")
-  , (17, "ctrl")
-  , (32, "space")
-  , (13, "enter")
-  , (35, "end")
-  , (36, "pos1")
-  , (45, "insert")
-  , (188, ",")
-  , (190, ".")
-  , (46, "del")
-  , (8, "bs")
-  , (9, "tab")
-  ]
-  ++ (List.map (toDefStrPair identity) [48..57]) -- top numbers
-  ++ (List.map (toDefStrPair identity) [96..105]) -- block numbers
-  ++ (List.map (toDefStrPair (\x -> x - 32)) [97..122]) -- a to z
-  |> Dict.fromList
+        (37, "left")
+      , (38, "up")
+      , (39, "right")
+      , (40, "down")
+      , (16, "shift")
+      , (17, "ctrl")
+      , (32, "space")
+      , (13, "enter")
+      , (35, "end")
+      , (36, "pos1")
+      , (45, "insert")
+      , (188, ",")
+      , (190, ".")
+      , (46, "del")
+      , (8, "bs")
+      , (9, "tab")
+      ]
+      ++ (List.map2 (,) [48..57] (List.map strFromCode [48..57])) -- top numbers
+      ++ (List.map2 (,) [96..105] (List.map strFromCode [48..57])) -- block numbers
+      ++ (List.map2 (,) [65..90] (List.map strFromCode [97..122])) -- a to z
+      |> Dict.fromList
 
 showKey : Keyboard.KeyCode -> String
 showKey key = case Dict.get key keyStrings of
