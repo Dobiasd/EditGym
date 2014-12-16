9cdee83e126299b60865c2861a4d6c8ac45241a3
Dobiasd <harry@daiw.de>
2014-12-16 22:45:19 +0100

    paragraphs

--- src/elm/Start.elm
+++ src/elm/Start.elm
@@ -19,7 +19,8 @@ helpButton = niceButton "Help" "?page=help"
 introduction : Int -> Element
 introduction w = Skeleton.showTextPart w <| Markdown.toElement """
 
-Sometimes you need to edit or rearrange text quickly. No matter if you use a [word processor](https://www.libreoffice.org/discover/writer) an [editor](http://notepad-plus-plus.org), an [IDE](https://eclipse.org)  or just write an email or a post on [facebook](http://www.facebook.com) or [something](http://www.reddit.com), the basic keyboard shortcuts are mainly the same.
+Sometimes you need to edit or rearrange text quickly. No matter if you use a [word processor](https://www.libreoffice.org/discover/writer), an [editor](http://notepad-plus-plus.org), an [IDE](https://eclipse.org)  or just write an email or a post on [facebook](http://www.facebook.com), the basic keyboard shortcuts are mostly the same.
+
 But [just memorizing them](https://www.shortcutfoo.com) alone is not enough to really [save time](http://lifehacker.com/5970089/back-to-the-basics-learn-to-use-keyboard-shortcuts-like-a-ninja).
 It is also important (and sometimes even fun) to choose the most efficient tactics effortlessly.
 
