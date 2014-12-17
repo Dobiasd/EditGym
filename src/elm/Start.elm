032b38a7d2813f49352a1f6a2d50193fae10e1b7
Dobiasd <harry@daiw.de>
2014-12-17 22:33:22 +0100

    link to jujis article

--- src/elm/Start.elm
+++ src/elm/Start.elm
@@ -21,7 +21,7 @@ introduction w = Skeleton.showTextPart w <| Markdown.toElement """
 
 Sometimes you need to edit or rearrange text quickly. No matter if you use a [word processor](https://www.libreoffice.org/discover/writer), an [editor](http://notepad-plus-plus.org), an [IDE](https://eclipse.org)  or just write an [email](https://www.mozilla.org/en-US/thunderbird/) or a post on [facebook](http://www.facebook.com), the basic keyboard shortcuts are mostly the same.
 
-But [just memorizing them](https://www.shortcutfoo.com) alone is not enough to really [save time](http://lifehacker.com/5970089/back-to-the-basics-learn-to-use-keyboard-shortcuts-like-a-ninja).
+But [just memorizing them](https://www.shortcutfoo.com) alone is not enough to really [save time](http://acrobolix.com/keyboarding-changed-my-life/).
 It is also important (and sometimes even fun) to choose the most efficient tactics effortlessly.
 
 EditGym helps you to acquire this set of skills by letting you practice and compete on standardized text snippets.
