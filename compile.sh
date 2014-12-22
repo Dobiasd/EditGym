cf61a683c403fc3b817f8f6466915d24a6cbcf2d
Dobiasd <harry@daiw.de>
2014-12-22 08:50:19 +0100

    rename main javascript file

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ rm -r build
 mkdir build
 mkdir build/js
 
-elm-make src/elm/Start.elm src/elm/Exercises.elm src/elm/Game.elm src/elm/Contact.elm src/elm/Help.elm src/elm/FAQ.elm src/elm/CreateExercise.elm src/elm/Newsletter.elm src/elm/PersonalBestList.elm --output build/js/bundle_uncompressed.js
+elm-make src/elm/Start.elm src/elm/Exercises.elm src/elm/Game.elm src/elm/Contact.elm src/elm/Help.elm src/elm/FAQ.elm src/elm/CreateExercise.elm src/elm/Newsletter.elm src/elm/PersonalBestList.elm --output build/js/editgym_uncompressed.js
 
 if [ $? -eq 0 ]
 then
@@ -13,8 +13,8 @@ then
   cp -r ./src/imgs ./build
   cp -R ./src/exercises ./build
 
-  uglifyjs build/js/bundle_uncompressed.js > build/js/bundle.js
-  rm build/js/bundle_uncompressed.js
+  uglifyjs build/js/editgym_uncompressed.js > build/js/editgym.js
+  rm build/js/editgym_uncompressed.js
 
   cp ./src/index.html ./build/index.html
   uglifyjs ./src/htmlmain.js > ./build/js/htmlmain.js
