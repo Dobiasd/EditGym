ca3c5528a6e3422b1d1db67104a8e0b81773f146
Dobiasd <harry@daiw.de>
2016-01-17 20:42:29 +0100

    new share links

--- src/elm/Header.elm
+++ src/elm/Header.elm
@@ -29,16 +29,16 @@ shareIcons =
     let
         buttons =
             [ ( image iconSize iconSize "imgs/thumb_up.png", "https://www.facebook.com/editgym" )
-            , ( image iconSize iconSize "imgs/facebook.png", genLink "https://www.facebook.com/sharer/sharer.php" [ ( "u", "http://www.editgym.com" ) ] )
-            , ( image iconSize iconSize "imgs/twitter.png", genLink "https://twitter.com/home" [ ( "status", "Text editing training at http://www.editgym.com" ) ] )
-            , ( image iconSize iconSize "imgs/googleplus.png", genLink "https://plus.google.com/share" [ ( "url", "http://www.editgym.com" ) ] )
-            , ( image iconSize iconSize "imgs/linkedin.png", genLink "https://www.linkedin.com/shareArticle" [ ( "mini", "true" ), ( "url", "http://www.editgym.com" ), ( "title", "Text editing training" ), ( "source", "" ) ] )
-            , ( image iconSize iconSize "imgs/pinterest.png", genLink "https://pinterest.com/pin/create/button/" [ ( "url", "" ), ( "media", "http://www.editgym.com" ), ( "description", "Text editing training" ) ] )
-            , ( image iconSize iconSize "imgs/digg.png", genLink "http://digg.com/submit" [ ( "phase", "2" ), ( "url", "http://www.editgym.com" ), ( "title", "Text editing training" ) ] )
-            , ( image iconSize iconSize "imgs/stumbleupon.png", genLink "http://www.stumbleupon.com/submit" [ ( "url", "http://www.editgym.com" ), ( "title", "Text editing training" ) ] )
+            , ( image iconSize iconSize "imgs/facebook.png", genLink "https://www.facebook.com/sharer/sharer.php" [ ( "u", "http://www.editgym.com/text-editing-training/" ) ] )
+            , ( image iconSize iconSize "imgs/twitter.png", genLink "https://twitter.com/home" [ ( "status", "Text editing training at http://www.editgym.com/text-editing-training/" ) ] )
+            , ( image iconSize iconSize "imgs/googleplus.png", genLink "https://plus.google.com/share" [ ( "url", "http://www.editgym.com/text-editing-training/" ) ] )
+            , ( image iconSize iconSize "imgs/linkedin.png", genLink "https://www.linkedin.com/shareArticle" [ ( "mini", "true" ), ( "url", "http://www.editgym.com/text-editing-training/" ), ( "title", "Text editing training" ), ( "source", "" ) ] )
+            , ( image iconSize iconSize "imgs/pinterest.png", genLink "https://pinterest.com/pin/create/button/" [ ( "url", "" ), ( "media", "http://www.editgym.com/text-editing-training/" ), ( "description", "Text editing training" ) ] )
+            , ( image iconSize iconSize "imgs/digg.png", genLink "http://digg.com/submit" [ ( "phase", "2" ), ( "url", "http://www.editgym.com/text-editing-training/" ), ( "title", "Text editing training" ) ] )
+            , ( image iconSize iconSize "imgs/stumbleupon.png", genLink "http://www.stumbleupon.com/submit" [ ( "url", "http://www.editgym.com/text-editing-training/" ), ( "title", "Text editing training" ) ] )
             , ( image iconSize iconSize "imgs/tumblr.png", genLink "http://www.tumblr.com/share/link" [ ( "url", "editgym.com" ) ] )
-            , ( image iconSize iconSize "imgs/bufferapp.png", genLink "https://bufferapp.com/add" [ ( "url", "http://www.editgym.com" ), ( "text", "Text editing training" ) ] )
-            , ( image iconSize iconSize "imgs/email.png", genLink "mailto: " [ ( "subject", "EditGym" ), ( "body", "Text editing training at http://www.editgym.com" ) ] )
+            , ( image iconSize iconSize "imgs/bufferapp.png", genLink "https://bufferapp.com/add" [ ( "url", "http://www.editgym.com/text-editing-training/" ), ( "text", "Text editing training" ) ] )
+            , ( image iconSize iconSize "imgs/email.png", genLink "mailto: " [ ( "subject", "EditGym" ), ( "body", "Text editing training at http://www.editgym.com/text-editing-training/" ) ] )
             ]
                 |> List.map (\( img, url ) -> img |> link url)
     in
